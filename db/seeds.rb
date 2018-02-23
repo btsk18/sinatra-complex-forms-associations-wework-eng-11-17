9607406ef2f36b69def1b93b546d41e09d8da782
Brandon Leeds <brandonleeds@gmail.com>
2018-02-23 01:40:04 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Add seed data here. Seed your database with `rake db:seed`
+brandon = Owner.create(name: "Brandon")
 sophie = Owner.create(name: "Sophie")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
