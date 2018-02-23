b7b57500aab98e615f58d981f488b81a1edfd100
Brandon Leeds <brandonleeds@gmail.com>
2018-02-23 01:13:31 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,7 +32,9 @@ class PetsController < ApplicationController
   end
 
   post '/pets/:id' do
-
+    @pet = Pet.find(params[:id])
+    @pet.update(params[:pet])
+    
     redirect to "pets/#{@pet.id}"
   end
 end
